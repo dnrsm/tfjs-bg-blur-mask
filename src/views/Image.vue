231970952d80fc77007737168399c106454e54ea
dnrsm <daikinarushima@gmail.com>
2020-07-14 00:57:45 +0900

    add ImageBg component

---
+++ src/views/Image.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ImageBg />
+  </div>
+</template>
+
+<script>
+import ImageBg from "@/components/ImageBg.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ImageBg
+  }
+};
+</script>
